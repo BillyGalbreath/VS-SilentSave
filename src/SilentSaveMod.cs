4c3445da9f345faea26d7eb781e4ca78ff734405
William Blake Galbreath <Blake.Galbreath@GMail.com>
2024-03-15 21:01:23 -0500

    cleanup project files

--- src/SilentSaveMod.cs
+++ src/SilentSaveMod.cs
@@ -8,22 +8,22 @@ using Vintagestory.Server;
 namespace SilentSave;
 
 public class SilentSaveMod : ModSystem {
-    private static SilentSaveMod instance = null!;
+    private static SilentSaveMod _instance = null!;
 
-    private readonly object @lock = new();
+    private readonly object _lock = new();
 
-    private bool doingAutoSave;
-    private bool doingOffThreadTickSave;
+    private bool _doingAutoSave;
+    private bool _doingOffThreadTickSave;
 
-    private Harmony? harmony;
+    private Harmony? _harmony;
 
     public SilentSaveMod() {
-        instance = this;
+        _instance = this;
     }
 
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     public bool SaveInProgress() {
-        lock (@lock) return doingAutoSave || doingOffThreadTickSave;
+        lock (_lock) return _doingAutoSave || _doingOffThreadTickSave;
     }
 
     public override bool ShouldLoad(EnumAppSide side) {
@@ -31,38 +31,38 @@ public class SilentSaveMod : ModSystem {
     }
 
     public override void StartServerSide(ICoreServerAPI api) {
-        harmony = new Harmony(Mod.Info.ModID);
-        harmony.Patch(typeof(ServerSystemAutoSaveGame).GetMethod("doAutoSave", BindingFlags.Instance | BindingFlags.NonPublic),
+        _harmony = new Harmony(Mod.Info.ModID);
+        _harmony.Patch(typeof(ServerSystemAutoSaveGame).GetMethod("doAutoSave", BindingFlags.Instance | BindingFlags.NonPublic),
             prefix: typeof(SilentSaveMod).GetMethod("PreDoAutoSave"),
             postfix: typeof(SilentSaveMod).GetMethod("PostDoAutoSave"));
-        harmony.Patch(AccessTools.TypeByName("Vintagestory.Server.ServerSystemLoadAndSaveGame").GetMethod("OnSeparateThreadTick", BindingFlags.Instance | BindingFlags.Public),
+        _harmony.Patch(AccessTools.TypeByName("Vintagestory.Server.ServerSystemLoadAndSaveGame").GetMethod("OnSeparateThreadTick", BindingFlags.Instance | BindingFlags.Public),
             prefix: typeof(SilentSaveMod).GetMethod("PreOnSeparateThreadTick"),
             postfix: typeof(SilentSaveMod).GetMethod("PostOnSeparateThreadTick"));
-        harmony.Patch(typeof(ServerLogger).GetMethod("LogImpl", BindingFlags.Instance | BindingFlags.NonPublic, new[] { typeof(EnumLogType), typeof(string), typeof(object[]) }),
+        _harmony.Patch(typeof(ServerLogger).GetMethod("LogImpl", BindingFlags.Instance | BindingFlags.NonPublic, new[] { typeof(EnumLogType), typeof(string), typeof(object[]) }),
             prefix: typeof(SilentSaveMod).GetMethod("PreLogImpl"));
     }
 
     public override void Dispose() {
-        harmony?.UnpatchAll(Mod.Info.ModID);
+        _harmony?.UnpatchAll(Mod.Info.ModID);
     }
 
     public static void PreDoAutoSave() {
-        lock (instance.@lock) instance.doingAutoSave = true;
+        lock (_instance._lock) _instance._doingAutoSave = true;
     }
 
     public static void PostDoAutoSave() {
-        lock (instance.@lock) instance.doingAutoSave = false;
+        lock (_instance._lock) _instance._doingAutoSave = false;
     }
 
     public static void PreOnSeparateThreadTick() {
-        lock (instance.@lock) instance.doingOffThreadTickSave = true;
+        lock (_instance._lock) _instance._doingOffThreadTickSave = true;
     }
 
     public static void PostOnSeparateThreadTick() {
-        lock (instance.@lock) instance.doingOffThreadTickSave = false;
+        lock (_instance._lock) _instance._doingOffThreadTickSave = false;
     }
 
     public static bool PreLogImpl() {
-        return !instance.SaveInProgress();
+        return !_instance.SaveInProgress();
     }
 }
